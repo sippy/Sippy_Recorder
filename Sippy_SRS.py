134a17c10349f1b918d7d032b46775ae8a317e51
Maksym Sobolyev <sobomax@sippysoft.com>
2023-09-28 19:16:52 +0000

    Update (c) notice to match reality.

--- Sippy_SRS.py
+++ Sippy_SRS.py
@@ -1,6 +1,4 @@
-# Copyright (c) 2018 Sippy Software, Inc. All rights reserved.
-#
-# All rights reserved.
+# Copyright (c) 2018-2023 Sippy Software, Inc. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification,
 # are permitted provided that the following conditions are met:
