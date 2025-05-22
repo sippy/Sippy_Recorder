a048efc6e2f7866a5fed614990d621c00391eeb6
Maksym Sobolyev <sobomax@sippysoft.com>
2025-05-22 10:32:01 -0700

    Add new parameters:
     o --ui enable experimental web-based UI.
     o --uiparams parameters to configure the UI.
     o --sip_port SIP port number to use.
    
    Add weak tracking of active sessions.
    
    Fix RTP session(s) setup logic whereby it will be using inverted
    direction thereby causing packets to be mirrored back to the SRC.

--- Sippy_SRS.py
+++ Sippy_SRS.py
@@ -22,6 +22,8 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from argparse import ArgumentParser
+from weakref import WeakSet
+from functools import partial
 
 from sippy.UA import UA
 from sippy.CCEvents import CCEventTry, CCEventConnect, CCEventFail
@@ -36,15 +38,16 @@ from sippy.Time.Timeout import Timeout
 from sippy.MsgBody import MsgBody
 from sippy.SdpBody import SdpBody
 from sippy.SipReason import SipReason
+from sippy.UI.Controller import UIController
 
 class SRSParams:
     sippy_c = None
     from_tag = None
     to_tag = None
     source = None
-    invite = None
     rsess = None
-    rtpp_res = None
+    rtpp_r_res = None
+    rtpp_u_res = None
     sess_sdp = None
     body_tmpl = '\r\n'.join(('v=0', f'o={SdpOrigin()}',
                              's=Sippy_SRS', 't=0 0'))
@@ -52,9 +55,9 @@ class SRSParams:
         self.sippy_c = sippy_c
         self.from_tag = req.getHFBody('from').getTag()
         self.source = req.getSource()
-        self.invite = req
         self.sess_sdp = []
-        self.rtpp_res = []
+        self.rtpp_r_res = []
+        self.rtpp_u_res = {}
 
 class SRSFailure(CCEventFail):
     c2m = {488:'Not Acceptable Here',
@@ -67,6 +70,7 @@ class SRSFailure(CCEventFail):
 
 class SippySRSUAS(UA):
     _p: SRSParams
+    cId: 'SipCallId'
 
     def __init__(self, sippy_c, req, sip_t):
         self._p = SRSParams(sippy_c, req)
@@ -75,13 +79,32 @@ class SippySRSUAS(UA):
 
     def sess_term(self, ua, rtime, origin, result = 0):
         print('disconnected')
+        self._p.rsess.delete()
+        del self._p.rsess
+        del self._p.sippy_c
+        self._p = None
 
-    def rtp_sess_created(self, result):
-        self._p.rtpp_res.append(result)
-        if len(self._p.rtpp_res) < len(self._p.sess_sdp):
+    def rtp_legA_created(self, index, result, _):
+        if result is None:
+            return self.rtp_rec_created(None)
+        up = RPC_up()
+        up.rtpps = self._p.rsess
+        up.index = index
+        up.result_callback = partial(self.rtp_legB_created, index)
+        self._p.rsess.callee.update(up)
+
+    def rtp_legB_created(self, index, result, _):
+        if result is None:
+            return self.rtp_rec_created(None)
+        self._p.rtpp_u_res[index] = (result.rtpproxy_address, result.rtpproxy_port)
+        self._p.rsess.start_recording(result_callback=self.rtp_rec_created, index=index)
+
+    def rtp_rec_created(self, result):
+        self._p.rtpp_r_res.append(result)
+        if len(self._p.rtpp_r_res) < len(self._p.sess_sdp):
             return
         nerrs = sum([1 if r is None or r.startswith('E') else 0
-                     for r in self._p.rtpp_res])
+                     for r in self._p.rtpp_r_res])
         if nerrs > 0:
             fail = SRSFailure(f'Just Can\'t, {nerrs} times', 502)
             self.recvEvent(fail)
@@ -89,10 +112,8 @@ class SippySRSUAS(UA):
         ah_pass = ('label', 'rtpmap', 'ptime')
         sdp = SdpBody(self._p.body_tmpl)
         for i, sdp_sect in enumerate(self._p.sess_sdp):
-            rsinfo = self._p.rsess.caller.rinfo_hst[i]
-            sdp_sect.c_header.addr = rsinfo.rtpproxy_address
             mh = sdp_sect.m_header
-            mh.port = rsinfo.rtpproxy_port
+            sdp_sect.c_header.addr, mh.port = self._p.rtpp_u_res[i]
             mh.formats = [f for i, f in enumerate(mh.formats) if i == 0]
             ah = sdp_sect.a_headers
             ah = [x for x in ah if x.name in ah_pass]
@@ -107,6 +128,7 @@ class SippySRSUAS(UA):
         if not isinstance(event, CCEventTry):
             return
         cId, cli, cld, mp_body, auth, caller_name = event.getData()
+        self.cId = cId
         if mp_body is None:
             self.recvEvent(SRSFailure('body-less INVITE is not supported (yet), open a PR!'))
             return
@@ -120,41 +142,46 @@ class SippySRSUAS(UA):
             return
         #print(type(sdp_body.content), type(sdp_body.content.sections))
         rs = Rtp_proxy_session(self._p.sippy_c, cId, self._p.from_tag, self._p.to_tag)
+        self._p.rsess = rs
         rs.caller.raddress = self._p.source
         for sdp in sdps:
             sdp.parse()
             for sect in sdp.content.sections:
-                def update_done(res, rtpps, _sect, index):
-                    if res is None:
-                        self.rtp_sess_created(None)
-                        return
-                    rs.start_recording(result_callback=self.rtp_sess_created,
-                                       index=index)
                 up = RPC_up()
                 up.rtpps = rs
                 up.index = len(self._p.sess_sdp)
-                up.result_callback = update_done
+                up.result_callback = partial(self.rtp_legA_created, up.index)
                 up.remote_ip = sect.c_header.addr
                 up.remote_port = sect.m_header.port
-                up.callback_parameters = (sect, up.index,)
-                rs.callee.update(up)
+                rs.caller.update(up)
                 self._p.sess_sdp.append(sect)
-        self._p.rsess = rs
 
 class SippySRS_Control(object):
     parser = ArgumentParser(description='Sippy SRS Control Interface')
-    parser.add_argument('--rtp_proxy_client', help='RTP proxy client spec string', default='/tmp/rtpp.sock')
+    parser.add_argument('--rtp_proxy_client', help='RTP proxy client spec string',
+                        default='/tmp/rtpp.sock')
+    parser.add_argument('--ui', action='store_true',
+                        help='Enable the web UI')
+    parser.add_argument('--uiparams', type=str, default='',
+                        help='UI parameters in key=value;key2=value2 format')
+    parser.add_argument('--sip_port', type=int, default=SipConf.my_port,
+                        help='SIP port to listen on')
     sippy_c = None
 
     def __init__(self):
         args = self.parser.parse_args()
         assert args.rtp_proxy_client is not None
-        print('RTP proxy client:', args.rtp_proxy_client)
         SipConf.my_uaname = 'Sippy SRS'
         logger = SipLogger(SipConf.my_uaname.replace(' ', '_'))
         sippy_c = {'_sip_address':SipConf.my_address,
-                   '_sip_port':SipConf.my_port,
+                   '_sip_port':args.sip_port,
                    '_sip_logger':logger}
+        self.sippy_c = sippy_c
+        self.active_uas = WeakSet()
+        if args.ui:
+            sippy_c['_cmap'] = self
+            sippy_c['uiparams'] = args.uiparams
+            UIController(sippy_c, "Sippy SIP Recording Server")
         udsc, udsoc = SipTransactionManager.model_udp_server
         udsoc.nworkers = 1
         rpc = Rtp_proxy_client(sippy_c, spath = args.rtp_proxy_client)
@@ -168,7 +195,6 @@ class SippySRS_Control(object):
         t.cancel()
         sippy_c['_rtp_proxy_clients'] = (rpc,)
         sippy_c['_sip_tm'] = SipTransactionManager(sippy_c, self.recvRequest)
-        self.sippy_c = sippy_c
 
     def run(self, timeout=None):
         return(ED2.loop(timeout))
@@ -180,8 +206,29 @@ class SippySRS_Control(object):
         if req.getMethod() == 'INVITE':
             # New dialog
             uaA = SippySRSUAS(self.sippy_c, req, sip_t)
+            self.active_uas.add(uaA)
             return
         return (req.genResponse(501, 'Not Implemented'), None, None)
+
+    def listActiveCalls(self):
+        rr = []
+        for ua in self.active_uas:
+            r = [str(ua.cId), 'Dummy']
+            (_h, _p), _t = ua.getRAddr0()
+            r.append((ua.state.sname, _t, _h, _p, ua.getCLD(), ua.getCLI()))
+            r.append(None)  # No uaO side
+            rr.append(tuple(r))
+        return tuple(rr)
+
+    @property
+    def ccmap(self):
+        return iter(self.active_uas)
+
+    def safeStop(self, signum=None):
+        for ua in self.active_uas:
+            ua.disconnect()
+        self.sippy_c['_sip_tm'].shutdown()
+        Timeout(ED2.breakLoop, 0.2, 1)
 
 if __name__ == '__main__':
     exit(SippySRS_Control().run())
